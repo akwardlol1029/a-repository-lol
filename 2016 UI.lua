8df3ed8c084808ebfab91c7778a96796171ae466
akwardlol1029 <adumb2717@gmail.com>
2024-08-17 11:28:44 +0800

    Update 2016 UI.lua

--- 2016 UI.lua
+++ 2016 UI.lua
@@ -111,11 +111,10 @@ TopBar.LeftFrame.MenuIcon.Background.Icon.Image = "rbxasset://textures/ui/Menu/H
 --// old graphics
 if config.old_graphics == true then
     --// epic 2016 remastered--// instances
-    local cc = Instance.new("ColorCorrectionEffect")
     local lighting = game:GetService("Lighting")
 
     --// hd killer
-    local ihateu = {"DepthOfFieldEffect", "SunRaysEffect", "BloomEffect", "BlurEffect", "ColorCorrectionEffect", "Atmosphere"}
+    local ihateu = {"DepthOfFieldEffect", "SunRaysEffect", "BloomEffect",, "Atmosphere"}
     for i, v in pairs(lighting:GetChildren()) do
         for index, value in ipairs(ihateu) do
             if v:IsA(value) then
@@ -125,11 +124,9 @@ if config.old_graphics == true then
     end
 
     --// setup
-    cc.Parent = game.Lighting
-    cc.Saturation = 0
-    cc.Contrast = -0.1
-    lighting.GlobalShadows = false
-
+    --lighting.GlobalShadows = false
+    lighting.Ambient = Color3.new(0.0823529, 0.0823529, 0.0823529)
+    lighting.OutdoorAmbient = Color3.new(0.5,0.5,0.5)
     sethiddenproperty(lighting, "Technology", Enum.Technology.Compatibility) 
 
     settings().Rendering.QualityLevel = 14
